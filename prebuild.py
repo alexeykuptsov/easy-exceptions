4851d9de2b42cf6da2abd365705f855667b8508c
Alexey Kuptsov <alxkuptsov@gmail.com>
2019-05-11 01:05:12 +0300

    Cleanup

--- prebuild.py
+++ prebuild.py
@@ -1,13 +1,8 @@
 import os
 import shutil
 
-import buildcommon
-
 if __name__ == '__main__':
     root_dir = os.path.dirname(__file__)
-    assembly_version_cs = 'src/AssemblyVersion.cs'
-    assembly_version_cs_bak = 'src/AssemblyVersion.cs.bak'
-
     shutil.copy(os.environ['ASSEMBLY_KEY_PATH'],
                 os.path.join(root_dir, 'key.snk'))
 
